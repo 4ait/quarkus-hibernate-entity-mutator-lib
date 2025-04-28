5d6868928dbab1c72b24eafe1a4c49e1c41b574f
Evgeniy Simonenko <tiikara93@gmail.com>
2025-04-28 18:35:33 +1000

    Add type constraints and convenience methods to HibernateEntityMutationSupport
    - Add type constraint to require entities to implement HibernateEntityMutationSupport
    - Add convenience extension methods for adding and removing collection elements
    - Improve type safety by enforcing that only entity objects can be used in relationships

--- src/main/kotlin/ru/code4a/quarkus/hibernate/mutator/interfaces/HibernateEntityMutationSupport.kt
+++ src/main/kotlin/ru/code4a/quarkus/hibernate/mutator/interfaces/HibernateEntityMutationSupport.kt
@@ -8,7 +8,7 @@ import ru.code4a.quarkus.hibernate.mutator.utils.nullable.unwrapElseError
 import kotlin.reflect.KProperty0
 
 interface HibernateEntityMutationSupport {
-  fun <V> mutatorRef(property: KProperty0<V>): HibernateRefMutator<V> {
+  fun <V : HibernateEntityMutationSupport?> mutatorRef(property: KProperty0<V>): HibernateRefMutator<V> {
     val currentClassNameWithFieldName =
       FindAllHibernateAssociationsInfoBuildStep.ClassNameWithFieldName(
         className = this::class.java.name,
@@ -24,11 +24,11 @@ interface HibernateEntityMutationSupport {
     return HibernateRefMutator(this, entityMutator)
   }
 
-  fun <V : Any?> KProperty0<V>.setRef(value: V) {
+  fun <V : HibernateEntityMutationSupport?> KProperty0<V>.setRef(value: V) {
     mutatorRef(this).set(value)
   }
 
-  fun <V : Any> mutatorRefs(property: KProperty0<MutableSet<V>>): HibernateCollectionMutator<V> {
+  fun <V : HibernateEntityMutationSupport> mutatorRefs(property: KProperty0<MutableSet<V>>): HibernateCollectionMutator<V> {
     val currentClassNameWithFieldName =
       FindAllHibernateAssociationsInfoBuildStep.ClassNameWithFieldName(
         className = this::class.java.name,
@@ -44,7 +44,15 @@ interface HibernateEntityMutationSupport {
     return HibernateCollectionMutator(this, entityMutator)
   }
 
-  fun <V : Any> KProperty0<MutableSet<V>>.setRefs(values: Collection<V>) {
+  fun <V : HibernateEntityMutationSupport> KProperty0<MutableSet<V>>.setRefs(values: Collection<V>) {
     mutatorRefs(this).set(values)
+  }
+
+  fun <V : HibernateEntityMutationSupport> KProperty0<MutableSet<V>>.add(value: V) {
+    mutatorRefs(this).add(value)
+  }
+
+  fun <V : HibernateEntityMutationSupport> KProperty0<MutableSet<V>>.remove(value: V) {
+    mutatorRefs(this).remove(value)
   }
 }
